b79f017aa94c926f51efcc241fee48b86e8b3da3
estrella <epopo2@illinois.edu>
2023-06-21 23:01:43 -0700

    add a form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
+'use client'
 import Image from 'next/image'
+import * as Yup from 'yup'
 import Logo from '../public/wece-logo-white.png'
+import { Formik } from 'formik'
 
 
 export default function Home() {
+  const validationSchema = Yup.object().shape({
+    longUrl: Yup.string().required("Required"),
+    suffix: Yup.string().required("Required")
+  })
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Image alt="wece logo" src={Logo} />
+    <main className="flex min-h-screen flex-col items-center">
+     <Image alt="wece logo" src={Logo} className="w-60 mt-10" />
+      <Formik initialValues={{longUrl: "", suffix: ""}} validationSchema={validationSchema} onSubmit={() => {}}
+    >
+        {({
+          values
+        }) =>(
+          <form className="mt-10 flex flex-col" >
+            <label className='font-Rubik' htmlFor="longurl">Paste Long URL </label>
+            <input id="longurl" name='longurl' spellCheck='false' type='url'></input>
+          </form>
+        )}
+      </Formik>
       
     </main>
   )
